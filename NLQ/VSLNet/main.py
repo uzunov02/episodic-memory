1c8551a8e3dd9a5a27b0264f2e335013ef567a21
uzunov02 <97812491+uzunov02@users.noreply.github.com>
2024-05-10 13:19:18 +0200

    Update main.py

--- NLQ/VSLNet/main.py
+++ NLQ/VSLNet/main.py
@@ -9,7 +9,7 @@ import torch
 import torch.nn as nn
 import submitit
 from torch.utils.tensorboard.writer import SummaryWriter
-from model.VSLNet import build_optimizer_and_scheduler, VSLNet
+from model.VSLBase import build_optimizer_and_scheduler, VSLBase
 from tqdm import tqdm
 from utils.data_gen import gen_or_load_dataset
 from utils.data_loader import get_test_loader, get_train_loader
@@ -98,7 +98,7 @@ def main(configs, parser):
             save_pretty=True,
         )
         # build model
-        model = VSLNet(
+        model = VSLBase(
             configs=configs, word_vectors=dataset.get("word_vector", None)
         ).to(device)
         optimizer, scheduler = build_optimizer_and_scheduler(model, configs=configs)
@@ -236,7 +236,7 @@ def main(configs, parser):
         parser.set_defaults(**pre_configs)
         configs = parser.parse_args()
         # build model
-        model = VSLNet(
+        model = VSLBase(
             configs=configs, word_vectors=dataset.get("word_vector", None)
         ).to(device)
 
